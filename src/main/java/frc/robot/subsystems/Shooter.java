5bc97c8a55d39fbe79b1afb3fcb7194022116496
JohnAbraham <55256167+Jrabps@users.noreply.github.com>
2026-01-27 19:57:19 -0500

    Added sysid to shooter and Indexer Added IndexerandShooter Subsystem

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,6 +8,7 @@ import java.util.function.Supplier;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VoltageOut;
@@ -19,6 +20,7 @@ import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.generated.TunerConstants;
@@ -26,19 +28,32 @@ import frc.robot.generated.TunerConstants;
 public class Shooter extends SubsystemBase {
     private final DutyCycleOut shooterVolts = new DutyCycleOut(0.0);
     private final VoltageOut sysIDVolt = new VoltageOut(0.0);
-
-
-    
     private final TalonFX shooterMotorL = new TalonFX(21, CANBus.roboRIO());
     private final TalonFX shooterMotorR = new TalonFX(20, CANBus.roboRIO());
-    public Shooter(int shooterMotorLID, int shooterMotorRID) {
 
+    public Shooter(int shooterMotorLID, int shooterMotorRID) {
+        var shooterMotorLConfig = new Slot0Configs();
+        shooterMotorLConfig.kP = 0.0;
+        shooterMotorLConfig.kI = 0.0;
+        shooterMotorLConfig.kD = 0.0;
+        shooterMotorLConfig.kS = 0.0;
+        shooterMotorLConfig.kV = 0.0;
+        shooterMotorLConfig.kA = 0.0;
+
+        shooterMotorL.getConfigurator().apply(shooterMotorLConfig);
         shooterMotorR.setControl(new Follower(shooterMotorRID, MotorAlignmentValue.Opposed));
     }
 
+    
     public void setControl(double output){
-
         shooterMotorL.setControl(shooterVolts.withOutput(output));
+    }
+
+    public Command getShooterCmd(double input) {
+        return this.runEnd(
+           () -> setControl(input),
+           () -> setControl(0.0)
+        );
     }
 
     private final SysIdRoutine shooterSysIdRoutine = 
@@ -54,5 +69,19 @@ public class Shooter extends SubsystemBase {
          this
         )
     );
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return shooterSysIdRoutine.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return shooterSysIdRoutine.dynamic(direction);
+    }
+
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("Index RPM", shooterMotorR.getVelocity().getValue().in(Rotations.per(Minute)));
+        SmartDashboard.putNumber("Index RPM", shooterMotorL.getVelocity().getValue().in(Rotations.per(Minute)));
+    }
 }
 
